49316e5223916531e7d704151d7f3a66e88bd7b7
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-02-20 17:07:55 -0300

    Add files via upload

---
+++ MemoryClass/memory.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+typedef uintptr_t PTR;
+
+namespace Mem
+{
+	namespace Ex
+	{
+		HANDLE GetProcessHandle(DWORD pid);
+		DWORD GetCurrentPID(const TCHAR* processName);
+		PTR GetModuleAddress(const TCHAR* moduleName, DWORD pid);
+		BOOL WriteBuffer(HANDLE hProc, PTR address, const void* value, SIZE_T size);
+		BOOL ReadBuffer(HANDLE hProc, PTR address, void* buffer, SIZE_T size);
+		PTR GetPointer(HANDLE hProc, PTR ptr, std::vector<PTR> offsets);
+	}
+	namespace In
+	{
+		HANDLE GetCurrentProcessHandle();
+		DWORD GetCurrentPID();
+		PTR GetModuleAddress(const char* moduleName);
+		bool WriteBuffer(PTR address, const void* value, SIZE_T size);
+		bool ReadBuffer(PTR address, void* buffer, SIZE_T size);
+		template <class type>
+		type Read(PTR address);
+		template <class type>
+		void Write(PTR address, type value);
+	}
+}
+
+template <class type>
+type Mem::In::Read(PTR address)
+{
+	return *(type*)(address);
+}
+
+template <class type>
+void Mem::In::Write(PTR address, type value)
+{
+	*(type*)(address) = value;
+}
